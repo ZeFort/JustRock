b094405a18d9b916e7df34c8275da7f6cc2ddc1b
Alex Saskevich <alexhomel@ya.ru>
2015-01-21 23:43:54 +0300

    Refactored code

--- routes/player.route.js
+++
@@ -1,8 +0,0 @@
-module.exports = function () {
-    var express = require('express');
-    var app = express();
-    app.get("/", function(req, res){
-        res.send("Player");
-    })
-    return app;
-}();
